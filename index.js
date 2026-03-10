70c1d0e8efc374f48297f8baabe633ba5ff74693
Ravil <182143436+hereexe@users.noreply.github.com>
2026-03-10 17:43:56 +0500

    add: task 2

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ function addListeners() {
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            fadeIn(block, 5000);
+            animaster().fadeIn(block, 5000);
         });
 
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
-            move(block, 1000, { x: 100, y: 10 });
+            animaster().move(block, 1000, { x: 100, y: 10 });
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
-            scale(block, 1000, 1.25);
+            animaster().scale(block, 1000, 1.25);
         });
 }
 
@@ -30,6 +30,12 @@ function animaster() {
         element.style.transitionDuration = `${duration}ms`;
         element.classList.remove('hide');
         element.classList.add('show');
+    }
+
+    function fadeOut(element, duration) {
+        element.style.transitionDuration = `${duration}ms`;
+        element.classList.remove('show');
+        element.classList.add('hide');
     }
 
     /**
@@ -52,12 +58,6 @@ function animaster() {
     function scale(element, duration, ratio) {
         element.style.transitionDuration = `${duration}ms`;
         element.style.transform = getTransform(null, ratio);
-    }
-
-    return {
-        fadeIn,
-        move,
-        scale
     }
 }
 
